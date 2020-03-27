84de869f4a303f0d57598e856ff292fd78fe2f55
Zachery Beal <bealanator@iCloud.com>
2020-03-26 21:14:25 -0500

    Removed unneded firewall rule files, changed permissions on wg0.conf

--- files/server_conf.sh
+++ files/server_conf.sh
@@ -10,3 +10,5 @@ PrivateKey = $( cat $keystore_path/server/privatekey )
 
 PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE ; ip6tables -A FORWARD -i wg0 -j ACCEPT; \
 ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE\n" > /opt/wireguard/conf_dir/wg0.conf
+
+chmod 660 /opt/wireguard/conf_dir/wg0.conf
