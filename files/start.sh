e9eb8afb913c2795b2cc1e4264d7a2cdcb352fc2
bealanator <62661922+bealanator@users.noreply.github.com>
2020-03-28 11:01:45 -0500

    Update start.sh
    Changed to host networking

--- files/start.sh
+++ files/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --detach --sysctl net.ipv6.conf.all.disable_ipv6=0 --sysctl net.ipv6.conf.all.forwarding=1 -v /opt/wireguard/conf_dir:/etc/wireguard --cap-add net_admin --cap-add sys_module --publish "51820:51820/udp" --restart=always --name wireguard wireguard
+docker run --network host --detach --sysctl net.ipv6.conf.all.disable_ipv6=0 --sysctl net.ipv6.conf.all.forwarding=1 -v /opt/wireguard/conf_dir:/etc/wireguard --cap-add net_admin --cap-add sys_module --restart=always --name wireguard wireguard
