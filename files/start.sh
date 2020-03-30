032f517f92222a18c9312052fc611738609bf5bc
Zachery Beal <bealanator@iCloud.com>
2020-03-30 15:18:04 -0500

    Fixed issues and typos

--- files/start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --network host --detach --sysctl net.ipv6.conf.all.disable_ipv6=0 --sysctl net.ipv6.conf.all.forwarding=1 -v /opt/wireguard/conf_dir:/etc/wireguard --cap-add net_admin --cap-add sys_module --restart=always --name wireguard wireguard
